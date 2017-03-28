322bc1e44a03accacf8dbfa4a6924caee4621fe8
vagrant <vagrant@homestead>
2017-03-28 11:45:24 +0000

    merge 28/3/2917

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -20,7 +20,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        //return view('home');
-        return view('group_form.info');
+        return view('home');
+        //return view('group_form.info');
     }
 }
