488eb0c4278b210d298ae85b5bdaba06b6886704
DonKu <dongku1993@gmail.com>
2017-04-22 15:44:24 +0800

    add newest, most popular, recommendation events pages

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,23 @@ namespace App\Http\Controllers;
 
 use Auth;
 use Illuminate\Http\Request;
+use Illuminate\Pagination\Paginator;
 use App\Http\Controllers\RecommendationTrait;
 
 class HomeController extends Controller
 {
     use RecommendationTrait;
+
+    protected function searchValidationArray()
+    {
+        return [
+                'time_from' => 'date|nullable',
+                'time_to' => 'date|nullable',
+                'event_name' => 'string|nullable',
+                'type' => 'integer|nullable',
+                'location' => 'integer|nullable'
+            ];
+    }
 
     /**
      * Create a new controller instance.
@@ -36,11 +48,71 @@ class HomeController extends Controller
     public function index()
     {
         $data = [
-                    'newest_events' => $this->newestEvents(),
-                    'most_popular_events' => $this->mostPopularEvents(),
+                    'newest_events' => $this->newestEvents(4),
+                    'most_popular_events' => $this->mostPopularEvents(4),
                     'recommend_events' => ($this->isLogin()) ? $this->recommendation_user_given(Auth::user()->id) : $this->randomJoinableEvents(4),
                 ];
 
         return view('home', $data);
+    }
+
+    public function newest_events(Request $request)
+    {
+        $this->validate($request, $this->searchValidationArray());
+
+        $keywords = $request->all();
+        $events = $this->newestEvents();
+        $events = new Paginator($events, 8);
+
+        foreach ($this->searchValidationArray() as $key => $value) {
+            $keywords[$key] = (!isset($keywords[$key]) || is_null($keywords[$key])) ? "" : $keywords[$key];
+        }
+
+        $data = [
+                'events' => $events,
+                'keywords' => (object)$keywords
+            ];
+
+        return view('event.list', $data);
+    }
+
+    public function most_popular_events(Request $request)
+    {
+        $this->validate($request, $this->searchValidationArray());
+
+        $keywords = $request->all();
+        $events = $this->mostPopularEvents();
+        $events = new Paginator($events, 8);
+
+        foreach ($this->searchValidationArray() as $key => $value) {
+            $keywords[$key] = (!isset($keywords[$key]) || is_null($keywords[$key])) ? "" : $keywords[$key];
+        }
+
+        $data = [
+                'events' => $events,
+                'keywords' => (object)$keywords
+            ];
+
+        return view('event.list', $data);
+    }
+
+    public function recommend_events(Request $request)
+    {
+        $this->validate($request, $this->searchValidationArray());
+
+        $keywords = $request->all();
+        $events = ($this->isLogin()) ? $this->sort_by_recommendation_user_given(Auth::user()->id) : $this->randomJoinableEvents(0);
+        $events = new Paginator($events, 8);
+
+        foreach ($this->searchValidationArray() as $key => $value) {
+            $keywords[$key] = (!isset($keywords[$key]) || is_null($keywords[$key])) ? "" : $keywords[$key];
+        }
+
+        $data = [
+                'events' => $events,
+                'keywords' => (object)$keywords
+            ];
+
+        return view('event.list', $data);
     }
 }
